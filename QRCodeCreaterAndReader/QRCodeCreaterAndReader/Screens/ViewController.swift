c398554ef3d61a7a00349fc7e646fd9177801ea0
BarkinSungu <barkinsungu@gmail.com>
2023-12-12 11:58:47 +0300

    Project created and folders edited.

---
+++ QRCodeCreaterAndReader/QRCodeCreaterAndReader/Screens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRCodeCreaterAndReader
+//
+//  Created by Barkın Süngü on 12.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
